ee31d50b024e7d1a4e71336ca05979e6ca9eee81
Jair Morocho <jairmorocho99@gmail.com>
2022-12-23 16:03:59 -0500

    first

---
+++ Views/Layouts/footer.php
@@ -0,0 +1,29 @@
+</div>
+  <!-- /.content-wrapper -->
+
+  <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.2.0
+    </div>
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="Util/Js/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="Util/Js/bootstrap.bundle.min.js"></script>
+<script src="Util/Js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="Util/Js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="Util/Js/demo.js"></script>
+</body>
+</html>
---
+++ Views/Layouts/footer_views.php
@@ -0,0 +1,10 @@
+<!-- jQuery -->
+<script src="../Util/Js/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../Util/Js/bootstrap.bundle.min.js"></script>
+<script src="../Util/Js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../Util/Js/adminlte.min.js"></script>
+
+</body>
+</html>
